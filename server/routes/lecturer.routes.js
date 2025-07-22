963d49e5091cc01f91ca27a434b88b2d6f82b33a
Fromb23 <rombo.programming@gmail.com>
2025-07-22 09:47:45 -0400

    feat(routes): define lecturer routes
    - GET /lecturer/:lecturerId to retrieve lecturer info
    - POST /lecturer/:lecturerId to update lecturer status
    - Protected by authMiddleware

--- server/routes/lecturer.routes.js
+++ server/routes/lecturer.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.middleware.js';
-import { getLecturers } from '../controllers/lecturer.controller.js';
+import { getLecturers,  getLecturerById, updateLecturerStatus } from '../controllers/lecturer.controller.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getLecturers);
+router.get('/:lecturerId', authMiddleware, getLecturerById);
+router.post('/:lecturerId', authMiddleware, updateLecturerStatus);
 
 export default router;
